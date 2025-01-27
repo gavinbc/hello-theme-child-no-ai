d507a21c919be2ce4f83730e70c59b41a1a39b6b
Gavin Coulson <github@gavincoulson.ca>
2025-01-26 16:16:25 -0800

    Update functions.php
    Added filter to disable ai by default

--- functions.php
+++ functions.php
@@ -34,3 +34,8 @@ function hello_elementor_child_scripts_styles() {
 
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
+
+function disable_elementor_ai() {
+    return null;
+}
+add_filter('get_user_option_' . Elementor\Modules\Ai\Preferences::ENABLE_AI, 'disable_elementor_ai');
